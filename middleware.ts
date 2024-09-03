7bc3cc3319e47cce1b1f31030b426adc68af4470
mercho40 <simon@mersich.net>
2024-09-03 18:05:03 -0300

    comentar middleware

--- middleware.ts
+++ middleware.ts
@@ -8,17 +8,17 @@ export async function middleware(req: NextRequest) {
   console.log("middleware: start");
 
   // Check if the user is authenticated
-  const authResult = await isAuthenticated(req);
-  console.log(authResult.isAuthenticated);
-
-  if (!authResult.isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
-    console.log("middleware: not Authenticated");
-    const loginUrl = new URL('/auth/signin', req.url);  // Specify your login page or authentication route
-    return NextResponse.redirect(loginUrl);
-  }
-
-  // Continue with the request if authenticated
-  return NextResponse.next();
+  //   const authResult = await isAuthenticated(req);
+  //   console.log(authResult.isAuthenticated);
+  //
+  //   if (!authResult.isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+  //     console.log("middleware: not Authenticated");
+  //     const loginUrl = new URL('/auth/signin', req.url);  // Specify your login page or authentication route
+  //     return NextResponse.redirect(loginUrl);
+  //   }
+  //
+  //   // Continue with the request if authenticated
+  //   return NextResponse.next();
 }
 
 export const config = {
