06e77b5faf6a1bfb3083499cd0cf132fca7950db
mercho40 <simon@mersich.net>
2024-09-19 21:56:46 -0300

    comentar middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 
 const protectedRoutes = ["/admin", "/profile"];
 
 export async function middleware(req: NextRequest) {
-  const session = await auth();
-  const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
-  if (isProtected && (!session || !session.user)) {
-    console.log("middleware: not Authenticated");
-    return NextResponse.redirect(new URL('/auth/signin', req.url));
-  }
+  // const session = await auth();
+  // const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
+  // if (isProtected && (!session || !session.user)) {
+  //   console.log("middleware: not Authenticated");
+  //   return NextResponse.redirect(new URL('/auth/signin', req.url));
+  // }
   return NextResponse.next();
 }
 
