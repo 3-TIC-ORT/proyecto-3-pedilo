3d14c5b81e4e8ec1a6504e17d3f13ff1750eb880
mercho40 <simonmer@protonnmail.com>
2024-10-02 11:00:30 -0300

    arreglar middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,26 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import authConfig from "./auth.config"
+import NextAuth from "next-auth"
 // import { auth } from "@/auth";
 
 const protectedRoutes = ["/admin", "/profile"];
 
-export async function middleware(req: NextRequest) {
-  // const session = await auth();
-  // const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
-  // if (isProtected && (!session || !session.user)) {
-  //   console.log("middleware: not Authenticated");
-  //   return NextResponse.redirect(new URL('/auth/signin', req.url));
-  // }
+const { auth } = NextAuth(authConfig)
+
+export default auth(async function middleware(req: NextRequest) {
+  // Your custom middleware logic goes here
+ const session = await auth();
+  const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
+  if (isProtected && (!session || !session.user)) {
+    console.log("middleware: not Authenticated");
+    return NextResponse.redirect(new URL('/auth/signin', req.url));
+  }
   return NextResponse.next();
-}
+})
+
+
+
 
 // Apply middleware to these routes
 export const config = {
