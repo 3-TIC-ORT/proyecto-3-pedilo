4078756c7cf55d3b94b31a709208d21b2273f72a
eitan-que <eitanfeldman021@gmail.com>
2024-10-09 20:40:07 -0300

    layout for every page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 
 export default function App() {
   return (
-    <div className="masterContainer">
-    </div>
+    <main>
+      <h1>U're now on "/". click on each route to go to it.</h1>
+      <ul>
+        <li><a href="/menu">/menu</a></li>
+        <li><a href="/auth/signin">/auth/signin</a></li>
+        <li><a href="/autn/signup">/autn/signup</a></li>
+        <li><a href="/cart">/cart</a></li>
+        <li><a href="/tables">/tables</a></li>
+        
+      </ul>
+    </main>
   );
 }
