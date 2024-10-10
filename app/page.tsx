f1b079b8d83cabfe1ce80db74305b5970b4a8359
eitan-que <eitanfeldman021@gmail.com>
2024-10-10 19:54:42 -0300

    fix changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function App() {
   return (
     <main>
-      <h1>U're now on "/". click on each route to go to it.</h1>
+      <h1>U&apos;re now on &quot;/&quot;. click on each route to go to it.</h1>
       <ul>
         <li><a href="/menu">/menu</a></li>
         <li><a href="/auth/signin">/auth/signin</a></li>
