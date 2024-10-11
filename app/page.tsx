ee8412bc3a0d35a0c53eefc9328c189cbb61e05d
eitan-que <eitanfeldman021@gmail.com>
2024-10-11 20:51:37 -0300

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,36 @@ import Image from "next/image";
 
 export default function App() {
   return (
-    <main>
-      <h1>U&apos;re now on &quot;/&quot;. click on each route to go to it.</h1>
-      <ul>
-        <li><a href="/api/auth/signin">/api/auth/signin</a></li>
-      </ul>
-    </main>
+    <>
+      <main>
+        <h1><a href="https://www.pedilo.tech/">Pedilo</a></h1>
+        <ul>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/profile">Perfil</a></li>
+        </ul>
+      </main>
+      <style>
+        {`
+          main {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 100%;
+          }
+          h1 {
+            font-size: 2rem;
+            font-weight: 900;
+          }
+          ul {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            list-style-type: none;
+          }
+        `}
+      </style>
+    </>
   );
 }
