a78220e833238dd9fe9b3d2f5e5badf2cec70afa
mercho40 <simon@mersich.net>
2024-10-11 17:04:49 -0300

    nombre y mail

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,31 +1,43 @@
 import React from 'react'
 import "./profile.css"
 import LogoutButton from '@/components/LogoutButton';
+import { auth } from '@/auth';
+import { getOrders } from '@/actions/order';
 
-function Profile() {
-  return (
-    <main>
+export async function Profile() {
+  const session = await auth();
+  if (!session) {
+    return (
+      <div>Not authenticated</div>
+    )
+  } else {
+    const user = session.user;
+    const orders = await getOrders(user.id);
+    return (
+      <main>
         <div className="info">
-            <img src="media/defaultProfilePicture.svg" alt="profilePicture" />
-            <div className="userInfo">
-                <div className="textLine">
-                    <p>Nombre:</p>
-                    <p>ejemplo</p>
-                </div>
-                <div className="textLine">
-                    <p>Email:</p>
-                    <p>ejemplo@gmail.com</p>
-                </div>
-                <div className="textLine">
-                    <p>Pedidos:</p>
-                    <p>99</p>
-                </div>
+          <img src="media/defaultProfilePicture.svg" alt="profilePicture" />
+          {user.image}
+          <div className="userInfo">
+            <div className="textLine">
+              <p>Nombre:</p>
+              <p>{user.name}</p>
             </div>
+            <div className="textLine">
+              <p>Email:</p>
+              <p>{user.email}</p>
+            </div>
+            <div className="textLine">
+              <p>Pedidos:</p>
+              <p>{orders}</p>
+            </div>
+          </div>
         </div>
         <button>Historial de pedidos</button>
         <LogoutButton />
-    </main>
-  )
+      </main>
+    )
+  }
 }
 
 export default Profile
