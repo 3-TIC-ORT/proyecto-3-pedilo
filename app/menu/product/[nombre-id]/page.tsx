b86a7be5cad687b8863aec0abdeed25e45780e2a
mercho40 <simon@mersich.net>
2024-10-10 23:01:48 -0300

    arreglar?

--- app/menu/product/[nombre-id]/page.tsx
+++ app/menu/product/[nombre-id]/page.tsx
@@ -2,6 +2,7 @@ import { getItems } from '@/actions/item'; // Import server action
 import ProductClient from './ProductClient'; // Client component for interactions
 import './product.css'; // Import styles
 import { Item } from '@prisma/client';
+import { auth } from '@/auth';
 
 interface ProductPageProps {
   params: { 'nombre-id': string }; // URL parameters
@@ -10,9 +11,11 @@ interface ProductPageProps {
 export default async function ProductPage({ params }: ProductPageProps) {
   const productId = parseInt(params['nombre-id'].split('-').pop() || '0', 10); // Extract and parse the product ID
   const product = await getItems(productId) as Item;
+  const session = await auth();
+  const userRole = session?.user.role;
 
   return (
-    <ProductClient product={product} />
+    <ProductClient product={product} userRole={userRole} />
   );
 }
 
