2e4d79838d82a9b712fbf0e8fe08b2a0b6e43dbb
mercho40 <simon@mersich.net>
2024-10-10 21:12:44 -0300

    arreglando middleware

--- auth.ts
+++ auth.ts
@@ -1,18 +1,12 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import authConfig from "./auth.config"
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend"
-import Credentials from "next-auth/providers/credentials";
 import { prisma } from "@/prisma";
-import { ZodError } from "zod";
-import { signInSchema } from "@/lib/definitions";
-import { compareSync } from 'bcrypt-edge';
 
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  ...authConfig,
   adapter: PrismaAdapter(prisma),
   debug: true,
   providers: [
@@ -28,72 +22,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           email: profile.email,
           image: profile.picture,
           role: profile.role ?? "user",
-        };
-      },
-    }),
-    Credentials({
-      id: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        try {
-          const { email, password } = await signInSchema.parseAsync(credentials);
-
-          // logic to verify if the user exists
-          const user = await prisma.user.findUnique({
-            where: { email: email },
-          });
-
-          if (user && user.password && await compareSync(password, user.password)) {
-            // Password matched, return user object
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              role: user.role,
-            };
-          }
-
-          // If user or password is incorrect, throw error
-          console.log("User not found or password mismatch.");
-          throw new Error("Invalid credentials");
-        } catch (error) {
-          if (error instanceof ZodError) {
-            console.log("Validation error:", error);
-            // Return null to indicate invalid credentials
-            return null;
-          }
-          throw error;
-        }
-      },
-    }),
-    Credentials({
-      id: "guest",
-      name: "Guest",
-      credentials: {
-        name: { label: "Name", type: "text" }
-      },
-      async authorize(credentials, req) {
-        if (!credentials?.name || typeof credentials.name !== 'string') {
-          throw new Error("Name is required for guest sign-in");
-        }
-
-        // Create a new guest user
-        const guestUser = await prisma.user.create({
-          data: {
-            name: credentials.name,
-            email: `guest_${Date.now()}@example.com`, // Unique email
-            role: "guest",
-          },
-        });
-
-        return {
-          id: guestUser.id,
-          email: guestUser.email,
-          name: guestUser.name,
-          role: guestUser.role,
         };
       },
     }),
--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,20 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-// import { auth } from "@/auth";
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { auth } from "@/auth";
 
-export const { auth: middleware } = NextAuth(authConfig)
-// const protectedRoutes = ["/admin", "/profile"];
+const protectedRoutes = ["/admin", "/profile"];
 
-//
-// export default auth((req) => {
-//   const session = !!req.auth;
-//   console.log(req.auth);
-//   const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
-//   if (isProtected && !session) {
-//     console.log("middleware: not Authenticated");
-//     return NextResponse.redirect(new URL('/auth/signin', req.url));
-//   }
-//   return NextResponse.next();
-// })
-//
-//
-
+export async function middleware(req: NextRequest) {
+  const session = await auth();
+  const isProtected = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route));
+  if (isProtected && (!session || !session.user)) {
+    console.log("middleware: not Authenticated");
+    return NextResponse.redirect(new URL('/auth/signin', req.url));
+  }
+  return NextResponse.next();
+}
 
 // Apply middleware to these routes
-// export const config = {
-//   matcher: ['/admin/:path*', '/profile/:path*']  // Protect /admin/* and /profile/* routes
-// }
-//
+export const config = {
+  matcher: ['/admin/:path*', '/profile/:path*']  // Protect /admin/* and /profile/* routes
+}
