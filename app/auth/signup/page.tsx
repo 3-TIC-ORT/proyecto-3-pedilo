6ef0f98c477615a823dfa1d20702c6c47b45974b
mercho40 <simon@mersich.net>
2024-10-10 21:46:38 -0300

    borrar signup

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link';
-import SignUpForm from '@/components/signUpForm';
+// import SignUpForm from '@/components/signUpForm';
 import './register.css';
 import { useRouter } from 'next/navigation';
 
@@ -11,10 +11,11 @@ export default function Page() {
     <main>
       <h1>Qué ganas de conocerte!</h1>
       <p>Ingresá tus datos y sé parte de nuestra familia.</p>
-      <SignUpForm />
-      <div className="otherActions">
-        <button onClick={() => router.back()} className="backBtn">Cancelar</button>
-      </div>
+      {/* <SignUpForm /> */}
+      {/* <div className="otherActions"> */}
+      {/*   <button onClick={() => router.back()} className="backBtn">Cancelar</button> */}
+      {/* </div> */}
+      <p>Esta pagina ya no funciona, para registrarse y logearse anda a signin</p>
     </main>
   );
 }
