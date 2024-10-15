e80d7704ddce2856c6fa60777b717bfd83d36a42
eitan-que <eitanfeldman021@gmail.com>
2024-10-14 22:57:43 -0300

    desing, not functional.
    mercho hace las actions lcdtpm

---
+++ app/tables/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./tables.css"
+
+function Tables() {
+  return (
+    <main>
+        <h1>Por favor selecciona una mesa disponible.</h1>
+        <section>
+            <button>
+                <p>Mesa 99</p>
+            </button>
+        </section>
+    </main>
+  )
+}
+
+export default Tables
