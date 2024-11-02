9f610a00311f23e43cc6038aa6d64145ffd8fe12
OliverJones08 <166547012+OliverJones08@users.noreply.github.com>
2024-11-02 15:23:54 -0300

    404 - Finished

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import "./not-found.css"
+
+export default function notFound (){
+    return(
+        <main className="text-left">
+         <h2 className="text-3xl">Error 404</h2>
+         <p>Te pedimos mil disculpas, hubo un problema en la aplicacion</p>
+         <p>Para reslverlo, chequeá que estés bien conectado a la red de internet</p>
+        </main>
+)
+}
