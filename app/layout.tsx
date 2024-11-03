34d6183870ed64d71237d1500d81048bb09b4514
mercho40 <simon@mersich.net>
2024-11-03 17:41:00 -0300

    volver atras

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { auth } from "@/auth";
 import { getUserTables } from "@/actions/tables"; // Importa la función
 import { hasPendingCall } from "@/actions/calls";
 import Link from "next/link";
-import Popups from "@/components/Popups";
 
 export const metadata: Metadata = {
   title: "Pedilo",
@@ -38,7 +37,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Popups/>
         <header>
           <img
             src="/images/logo.svg"
