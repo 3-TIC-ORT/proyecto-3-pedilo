a042758f5ee87e2a799bb8ec47053a7e5e81758a
mercho40 <mercho40@example.com>
2024-11-06 15:27:26 -0300

    change ably to realtime

--- lib/ably.ts
+++ lib/ably.ts
@@ -1,3 +1,3 @@
 import * as Ably from 'ably'
 
-export const ablyClient = new Ably.Realtimet({ key: process.env.ABLY_API_KEY });
+export const ablyClient = new Ably.Realtime({ key: process.env.ABLY_API_KEY });
