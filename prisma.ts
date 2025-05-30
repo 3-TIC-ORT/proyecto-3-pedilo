38ded9f245df6727e8fed77e610d8dd3e8ef44a4
mercho40 <107892036+mercho40@users.noreply.github.com>
2025-05-30 11:19:16 -0300

    update

--- prisma.ts
+++ prisma.ts
@@ -3,7 +3,7 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 import { Pool } from '@neondatabase/serverless'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-const neon = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL })
+const neon = new Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaNeon(neon)
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
